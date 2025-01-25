61a99f9ac820431c9e6d1ba03278e69d7e2afdbb
rinay06 <ry06@uw.edu>
2025-01-25 09:36:31 -0800

    Add files via upload

---
+++ GroceryPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GroceryPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
