5c7d04cdb72e9e1995e5d58527d5e3907a048856
rinay06 <ry06@uw.edu>
2025-01-25 10:37:12 -0800

    Initial commit

---
+++ src/main/java/com/example/GroceryPlanner/GroceryPlannerApplication.java
@@ -0,0 +1,14 @@
+package com.example.GroceryPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryPlannerApplication.class, args);
+	}
+
+}
+
